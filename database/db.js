6e1fe3cc61487f65fc3c811d1822a82272598afb
TsCJbot <107740890+TsCJbot@users.noreply.github.com>
2023-03-22 15:27:39 +0530

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,5 +1,5 @@
 /**
-* @project_name Queen Amdi [WA Multi-device]
+* @project_name CJ-MD [WA Multi-device]
 * @author BlackAmda <https://github.com/BlackAmda>
 * @description A WhatsApp based 3ʳᵈ party application that provide many services with a real-time automated conversational experience
 * @link <https://github.com/BlackAmda/QueenAmdi>
